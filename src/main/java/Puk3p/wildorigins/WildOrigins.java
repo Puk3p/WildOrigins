3724896700d39e41d512794f036b08adbb8c3b19
Lupu Gheorghe <gheorghe.lupu2@student.tuiasi.ro>
2025-02-16 14:09:57 +0200

    Created abilities.yml, config.yml and messages.yml and loaded to server.

--- src/main/java/Puk3p/wildorigins/WildOrigins.java
+++ src/main/java/Puk3p/wildorigins/WildOrigins.java
@@ -1,16 +1,24 @@
 package Puk3p.wildorigins;
 
+import Puk3p.wildorigins.utils.ConfigManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WildOrigins extends JavaPlugin {
 
+    private ConfigManager configManager;
+
     @Override
     public void onEnable() {
+        configManager = new ConfigManager(this);
         getLogger().info("WildOrigins a fost activat!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("WildOrigins a fost dezactivat!");
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
     }
 }
--- src/main/java/Puk3p/wildorigins/utils/ConfigManager.java
+++ src/main/java/Puk3p/wildorigins/utils/ConfigManager.java
@@ -1,4 +1,78 @@
 package Puk3p.wildorigins.utils;
 
+import Puk3p.wildorigins.WildOrigins;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
 public class ConfigManager {
+    private final WildOrigins plugin;
+    private FileConfiguration config, messages, abilities;
+    private File configFile, messageFile, abilitiesFile;
+
+    public ConfigManager(WildOrigins plugin) {
+        this.plugin = plugin;
+        loadFiles();
+    }
+
+    private void loadFiles() {
+        //loading each config file
+        configFile = new File(plugin.getDataFolder(), "config.yml");
+        messageFile = new File(plugin.getDataFolder(), "messages.yml");
+        abilitiesFile = new File(plugin.getDataFolder(), "abilities.yml");
+
+        //create if not exist
+        if (!configFile.exists()) plugin.saveResource("config.yml", false);
+        if (!messageFile.exists()) plugin.saveResource("messages.yml", false);
+        if (!abilitiesFile.exists()) plugin.saveResource("abilities.yml", false);
+
+        //load it
+        config = YamlConfiguration.loadConfiguration(configFile);
+        messages = YamlConfiguration.loadConfiguration(messageFile);
+        abilities = YamlConfiguration.loadConfiguration(abilitiesFile);
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+    public FileConfiguration getMessages() {
+        return messages;
+    }
+
+    public FileConfiguration getAbilities() {
+        return abilities;
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "Could not save config", e);
+        }
+    }
+
+    public void saveMessages() {
+        try {
+            messages.save(messageFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "Could not save messages", e);
+        }
+    }
+
+    public void saveAbilities() {
+        try {
+            abilities.save(abilitiesFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "Could not save abilities", e);
+        }
+    }
+
+    public void reloadFiles() {
+        loadFiles();
+        plugin.getLogger().info("Reloaded config");
+    }
 }
